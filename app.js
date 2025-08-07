31949ce7f0a342f5cdf24253cc9259ecc5be41ac
calderflor <calder_83@hotmail.com>
2025-08-07 17:35:46 -0300

    crea primeras funciones

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//crear array donde voy a guardad los nombres
+let nombres = [];           
+//obtener elementos del DOM
+let input = document.getElementById("amigo");
+let boton = document.getElementById("agregarAmigo()");
+let lista = document.getElementById("listaAmigos");
 
-// No te preocupes por la implementación, solo enfócate en el algoritmo.
-// Definir una función que resuelva el problema
-function resolverProblema() {
-    // Definir variables
-    let a = 5;
-    let b = 10;
-    let c = 15;
-    let d = 20;
-}
-//agregando comentario de prueba
+//crear funcion para agregar un nombre al array
+//leer el input
+//verificar si el input no esta vacio
+//si no esta vacio agregar el nombre al array
+//limpia el input
+//llama a la funcion que actualiza lista en pantalla                                                                      
+ function agregarAmigo(){
+    let nombre = input.value;
+    if(nombre != ""){
+        nombres.push(nombre);
+        input.value = "";
+        actualizarLista();
+        }
+ }
+//crear una funcion para mostrar la lista actualizada en pantalla
+function actualizarLista(){
+    lista.innerHTML = "";
+    for(let i = 0; i < nombres.length; i++){
+        let li = document.createElement("li");
+        li.textContent = nombres[i];
+        lista.appendChild(li);
+        }
+    }
+
+//conectar el boton con la funcion
