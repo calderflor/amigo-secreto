dca0dbf06090803bb770b0dcf9e9784f4e149798
calderflor <calder_83@hotmail.com>
2025-08-08 17:53:58 -0300

    actualizacion 2

--- app.js
+++ app.js
@@ -1,32 +1,36 @@
-//crear array donde voy a guardad los nombres
-let nombres = [];           
-//obtener elementos del DOM
-let input = document.getElementById("amigo");
-let boton = document.getElementById("agregarAmigo()");
-let lista = document.getElementById("listaAmigos");
+let amigos =[];
+let nombreAmigos = document.getElementById("amigo");
+let listaAmigos = document.getElementById("listaAmigos");
+let resultado = document.getElementById("resultado");
 
-//crear funcion para agregar un nombre al array
-//leer el input
-//verificar si el input no esta vacio
-//si no esta vacio agregar el nombre al array
-//limpia el input
-//llama a la funcion que actualiza lista en pantalla                                                                      
- function agregarAmigo(){
-    let nombre = input.value;
-    if(nombre != ""){
-        nombres.push(nombre);
-        input.value = "";
-        actualizarLista();
-        }
- }
-//crear una funcion para mostrar la lista actualizada en pantalla
-function actualizarLista(){
-    lista.innerHTML = "";
-    for(let i = 0; i < nombres.length; i++){
-        let li = document.createElement("li");
-        li.textContent = nombres[i];
-        lista.appendChild(li);
-        }
+function agregarAmigos() {
+    let nombre = nombreAmigos.value;
+    if (nombre == 0) {
+        alert("Por favor, inserte un nombre");
+    } 
+    amigos.push(nombre);
+    document.getElementById("amigo").value;
+    mostrarLista;    
+}
+
+function mostrarLista() {
+    // limpiar lista html
+    listaAmigos.innerHTML = "";
+    //recorrer el array con for
+    for (let i = 0; i < amigos.length; i++) {
+    //crear una <li> por amigo
+    let item = document.createElement("li");
+    item.textContent = amigos[i];
+    listaAmigos.appendChild(item);
+    //insertar en el html
     }
+}
 
-//conectar el boton con la funcion
+function mostrarAmigo() {
+    //validar array no vacio
+    if (amigos.length > 0) {
+        //generar indice aleatorio
+        let indice = Math.floor(Math.random() * amigos.length);
+        //mostrar nombre sorteado
+        resultado.textContent = amigos[indice];    
+}
