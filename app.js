b8e70b4f02a91ffcff4f98c9bc3e570243ba599a
calderflor <calder_83@hotmail.com>
2025-08-10 15:18:54 -0300

    versión final de la lógica para agregar, validar y sortear amigos

--- app.js
+++ app.js
@@ -3,34 +3,55 @@ let nombreAmigos = document.getElementById("amigo");
 let listaAmigos = document.getElementById("listaAmigos");
 let resultado = document.getElementById("resultado");
 
-function agregarAmigos() {
+let btnAñadir = document.getElementById("Añadir");
+btnAñadir.addEventListener("click", agregarAmigo);
+
+let btnSortear = document.getElementById("Sortear");
+btnSortear.addEventListener("click", mostrarAmigo);
+
+function agregarAmigo() {
     let nombre = nombreAmigos.value;
-    if (nombre == 0) {
+      
+    if (nombre === "") {
         alert("Por favor, inserte un nombre");
+        return; 
     } 
-    amigos.push(nombre);
-    document.getElementById("amigo").value;
-    mostrarLista;    
+    
+    if (soloLetras(nombre)) {
+     amigos.push(nombre);
+} else {
+    alert("Solo se permiten letras y espacios");
+    return;
+}
+    nombreAmigos.value = "";
+    mostrarLista();    
 }
 
 function mostrarLista() {
-    // limpiar lista html
     listaAmigos.innerHTML = "";
-    //recorrer el array con for
+   
     for (let i = 0; i < amigos.length; i++) {
-    //crear una <li> por amigo
     let item = document.createElement("li");
     item.textContent = amigos[i];
     listaAmigos.appendChild(item);
-    //insertar en el html
     }
 }
 
 function mostrarAmigo() {
-    //validar array no vacio
-    if (amigos.length > 0) {
-        //generar indice aleatorio
-        let indice = Math.floor(Math.random() * amigos.length);
-        //mostrar nombre sorteado
-        resultado.textContent = amigos[indice];    
+    
+    if (amigos.length === 0) {
+       
+        alert("No hay amigos para sortear");
+        return;
+    }  let indice = Math.floor(Math.random() * amigos.length);
+       let amigoSorteado = amigos[indice];
+       resultado.textContent = `\uD83C\uDF89 Amigo sorteado: ${amigoSorteado}`;
+
+       listaAmigos.innerHTML = "";
+      
+       amigos = [];
+}
+
+function soloLetras(texto) {
+  return /^[a-zA-Z\s]+$/.test(texto);
 }
